3d3b851f27562b6adddb0fa4b4ee83f473234181
Maggie Sleziak <maggie@cosmic-maggiepro.cosmic.ucar.edu>
2023-05-11 19:11:47 -0600

    fixing path

--- python-files/getData.py
+++ python-files/getData.py
@@ -13,19 +13,20 @@ import tarfile
 
 
 url = 'https://data.cosmic.ucar.edu/gnss-ro/spire/noaa/nrt/level1b/2023/110/podTec_nrt_2023_110.tar.gz'
-target_path = 'GNSS-data/podTec_nrt_2023_110.tar.gz'
+target_path = '../GNSS-data/cosmic2/podTc2/podTec_nrt_2023_110.tar.gz'
 
 response = requests.get(url, stream=True)
 if response.status_code == 200:
     with open(target_path, 'wb') as f:
         f.write(response.raw.read())
 
+data_dir = '../GNSS-data/cosmic2/podTc2/'
 
 # open file
-file = tarfile.open('GNSS-data/podTec_nrt_2023_110.tar.gz')
+file = tarfile.open(target_path)
   
 # extracting file
-file.extractall('./GNSS-data')
+file.extractall(data_dir)
   
 file.close()
 
